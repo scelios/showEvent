96926933afa556a200b7eb2543daf7a80bda2720
scelios <scelios@outlook.fr>
2026-03-30 11:01:47 +0200

    Silenced warning of scss deprecated

--- pkg/linkedressources/vue.config.js
+++ pkg/linkedressources/vue.config.js
@@ -7,4 +7,34 @@ base.parallel = false;
 // Source maps increase memory & output size; usually not needed for packaging
 base.productionSourceMap = false;
 
+// Suppress Sass deprecation warnings coming from shell dependency styles.
+base.css = base.css || {};
+base.css.loaderOptions = base.css.loaderOptions || {};
+const existingSassLoaderOptions = base.css.loaderOptions.sass || {};
+const existingScssLoaderOptions = base.css.loaderOptions.scss || {};
+
+const sassDeprecationOptions = {
+  quietDeps: true,
+  silenceDeprecations: ['import', 'if-function', 'legacy-js-api', 'global-builtin', 'slash-div']
+};
+
+base.css.loaderOptions.sass = {
+  ...existingSassLoaderOptions,
+  sassOptions: {
+    ...(existingSassLoaderOptions.sassOptions || {}),
+    ...sassDeprecationOptions
+  }
+};
+
+const scssAdditionalData = existingScssLoaderOptions.additionalData || existingSassLoaderOptions.additionalData;
+
+base.css.loaderOptions.scss = {
+  ...existingScssLoaderOptions,
+  ...(scssAdditionalData ? { additionalData: scssAdditionalData } : {}),
+  sassOptions: {
+    ...(existingScssLoaderOptions.sassOptions || {}),
+    ...sassDeprecationOptions
+  }
+};
+
 module.exports = base;
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,37 @@
 const config = require('@rancher/shell/vue.config'); // eslint-disable-line @typescript-eslint/no-var-requires
 
-module.exports = config(__dirname, {
+const base = config(__dirname, {
   excludes: [],
   // excludes: ['fleet', 'example']
 });
+
+base.css = base.css || {};
+base.css.loaderOptions = base.css.loaderOptions || {};
+const existingSassLoaderOptions = base.css.loaderOptions.sass || {};
+const existingScssLoaderOptions = base.css.loaderOptions.scss || {};
+
+const sassDeprecationOptions = {
+  quietDeps: true,
+  silenceDeprecations: ['import', 'if-function', 'legacy-js-api', 'global-builtin', 'slash-div']
+};
+
+base.css.loaderOptions.sass = {
+  ...existingSassLoaderOptions,
+  sassOptions: {
+    ...(existingSassLoaderOptions.sassOptions || {}),
+    ...sassDeprecationOptions
+  }
+};
+
+const scssAdditionalData = existingScssLoaderOptions.additionalData || existingSassLoaderOptions.additionalData;
+
+base.css.loaderOptions.scss = {
+  ...existingScssLoaderOptions,
+  ...(scssAdditionalData ? { additionalData: scssAdditionalData } : {}),
+  sassOptions: {
+    ...(existingScssLoaderOptions.sassOptions || {}),
+    ...sassDeprecationOptions
+  }
+};
+
+module.exports = base;
